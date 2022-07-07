886ffee7de124dab7b4f57f28b5f7d792a2eaab6
umeshkhatiwada13 <umesh.khatiwada@infodevelopers.com.np>
2022-07-07 06:44:05 +0545

    Verifying commit

---
+++ src/main/java/com/security/demo/student/Student.java
@@ -0,0 +1,24 @@
+package com.security.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author umeshkhatiwada13@infodev
+ * @project demo1
+ * @created 06/07/2022 - 5:37 PM
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private Integer id;
+    private String name;
+
+    // Write a program to find odd numbers between 1 to 100.
+
+}
---
+++ src/main/java/com/security/demo/student/StudentController.java
@@ -0,0 +1,32 @@
+package com.security.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author umeshkhatiwada13@infodev
+ * @project demo1
+ * @created 06/07/2022 - 5:36 PM
+ */
+@RestController
+@RequestMapping("api/v1/student")
+public class StudentController {
+
+    private static final List<Student> STUDENT_LIST = Arrays.asList(
+            new Student(1, "Ram"),
+            new Student(2, "Shyam"),
+            new Student(3, "Hari")
+    );
+
+    @GetMapping("{id}")
+    public Student getStudent(@PathVariable Integer id) {
+        return STUDENT_LIST.stream().filter(student ->
+                id.equals(student.getId())).findFirst().orElseThrow(() -> new IllegalArgumentException("Student with Id " + id + " not found"))
+                ;
+    }
+}
